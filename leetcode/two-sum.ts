2cfd9bf964ae315564dc551ca6f26c14af5fcaa1
Bruno Moreira <bruno.moreira@cargon.com.br>
2024-12-18 20:38:02 -0300

    feat: desafios leet code

---
+++ leetcode/two-sum.ts
@@ -0,0 +1,10 @@
+const twoSums = function (nums: number[], target: number) {
+  const map = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    if (map.has(complement)) return [i, map.get(complement)];
+
+    map.set(nums[i], i);
+  }
+};
